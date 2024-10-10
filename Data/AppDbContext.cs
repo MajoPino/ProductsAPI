c0d3c3a5b896c93e426e55c00d49c26e59e9d1b0
MajoPino <majo12pino@gmail.com>
2024-10-10 08:44:02 -0500

    feat: Creation of the constructor of the AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,5 +3,5 @@ using Microsoft.EntityFrameworkCore;
 namespace ProductsAPI.Data;
 public class AppDbContext : DbContext
 {
-
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
