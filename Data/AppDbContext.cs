8e090fb27dac3597662c8c6c2526d3b22a98b887
MajoPino <majo12pino@gmail.com>
2024-10-10 09:19:00 -0500

    feat: Set the class Category into the AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Models;
 
 namespace ProductsAPI.Data;
 public class AppDbContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
