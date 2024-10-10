05ff23723c6e3233b91daeca8a5ea068a46e68cd
MajoPino <majo12pino@gmail.com>
2024-10-10 09:11:48 -0500

    feat: Adding the data annotations and the constructors

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ProductsAPI.Models;
 
+[Table("categories")]
 public class Category
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
+    [Required]
+    [MaxLength(100)]
     public required string Name { get; set; }
+
+    [Column("description")]
+    [MaxLength(400)]
     public string? Description { get; set; }
+
+    public Category(){}
+
+    public Category(int id, string name, string description)
+    {
+        Id = id;
+        Name = name.ToLower().Trim();
+        Description = description.ToLower().Trim();
+    }
 }
