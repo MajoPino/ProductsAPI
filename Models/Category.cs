8d40fe93c1ad03028a5658bb670857b44fe1eb61
MajoPino <majo12pino@gmail.com>
2024-10-10 09:00:24 -0500

    feat: Creation of the model Category

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+namespace ProductsAPI.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
