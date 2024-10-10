b8c9a571474d585fc0f0b5ccba0b215ab244caa8
MajoPino <majo12pino@gmail.com>
2024-10-10 11:45:02 -0500

    feat: Creating the Categories GET controllers

---
+++ Controllers/V1/Categories/CategoriesGetController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Repositories;
+
+namespace ProductsAPI.Controllers.V1.Categories;
+
+[ApiController]
+[Route("api/v1/categories")]
+public class CategoriesGetController : CategoriesController
+{
+    public CategoriesGetController(ICategoryRepository categoryRepository) : base(categoryRepository){}
+
+    //GET: /api/v1/categories
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        try
+        {
+            var categories = await _categoryRepository.GetAll();
+            return Ok(categories);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "An unexpected error occurred.");
+        }
+    }
+
+    //GET: api/v1/categories/{id}
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCategoryById(int id)
+    {
+        try
+        {
+            var category = await _categoryRepository.GetById(id);
+            if (category == null)
+            {
+                return NotFound($"Category with id {id} not found.");
+            }
+            return Ok(category);
+        }
+        catch (ArgumentNullException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "An unexpected error occurred.");
+        }
+    }
+}
